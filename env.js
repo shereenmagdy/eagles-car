204fbad3deb6bea3fb1920ce4a28fe0878861dcb
Shereen Magdy <99685762+shereenmagdy@users.noreply.github.com>
2024-05-26 19:03:21 +0300

    Add files via upload

---
+++ env.js
@@ -0,0 +1,6 @@
+// moved env variables into js file for codesandbox because it doesn't support .env
+export default {
+  VUE_APP_API_URL: "http://localhost:3000",
+  VUE_APP_FACEBOOK_APP_ID: "304757055637865",
+};
+//869883691448275
